af2026af563472dc4f69ebfdfd35283b61d8196e
Althea Janine Duran <112060534+Algessi@users.noreply.github.com>
2023-02-01 12:17:23 +0800

    Add files via upload

--- activity1.java
+++ activity1.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
+ 
 public class activity1 {
-    public static void main(String[] args) {
-        Scanner values = new Scanner(System.in);
-
-        System.out.print("Input first number: ");
-        int firstValue = values.nextInt();
-
-        System.out.print("Input second number: ");
-        int secondValue = values.nextInt();
-
-        int result = firstValue + secondValue;
-        String binary1 = Integer.toBinaryString(firstValue);
-        String binary2 = Integer.toBinaryString(secondValue);
-        String binaryAns = Integer.toBinaryString(result);
-
-        System.out.println(firstValue + " = " + binary1);
-        System.out.println(secondValue + " = " + binary2);
-        System.out.println(result + " = " + binaryAns);   
-    }
-    }
+ 
+ public static void main(String[] args) {
+  Scanner num = new Scanner(System.in);
+   
+  System.out.print("Input first number: ");
+  int num1 = num.nextInt();
+   
+  System.out.print("Input second number: ");
+  int num2 = num.nextInt();
+   
+ 
+  System.out.println(num1 + " + " + num2 + " = " + 
+  (num1 + num2));
+   
+  System.out.println(num1 + " - " + num2 + " = " + 
+  (num1 - num2));
+   
+  System.out.println(num1 + " x " + num2 + " = " + 
+  (num1 * num2));
+   
+  System.out.println(num1 + " / " + num2 + " = " + 
+  (num1 / num2));
+ 
+  System.out.println(num1 + " mod " + num2 + " = " + 
+  (num1 % num2));
+ }
+}
 
 
  
